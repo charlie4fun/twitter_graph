7dbd1d02a38efb1fa59067ca91e798a30eb4ad4e
charlie4fun <isakov.ilya@gmail.com>
2017-04-20 22:18:49 +0300

    example piece loaded

--- twitter_stream.py
+++ twitter_stream.py
@@ -5,18 +5,19 @@ from tweepy.streaming import StreamListener
 
 consumer_key = "i2MOcYvFUBYyV2VLstIxQWFox"
 consumer_secret = "3snc68OiPRMACizWvkz7O8GHoHZGqrTnsC8WPC8QLvRtA0OIfS"
-access_token= "748433808584695808-4dwGAIBSd6MNRKqseVx6YU5L6Zz7Zty"
+access_token = "748433808584695808-4dwGAIBSd6MNRKqseVx6YU5L6Zz7Zty"
 access_secret = "9EKE0xI9uustkbxlQV14tZe2J7V1zf7TvZ3QXqPlL1B2J"
 
 auth = tweepy.OAuthHandler(consumer_key=consumer_key, consumer_secret=consumer_secret)
 
 api = tweepy.API(auth)
 
-class listener(StreamListener):
+
+class Listener(StreamListener):
 
     def on_data(self, data):
         print(data)
-        return(True)
+        return True
 
     def on_error(self, status):
         print(status)
@@ -24,5 +25,5 @@ class listener(StreamListener):
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
-twitterStream = Stream(auth, listener())
+twitterStream = Stream(auth, Listener())
 twitterStream.filter(track=['kereta', 'mobil', 'gerbong', 'gerobak', 'oto', 'wagon', 'pedati'])

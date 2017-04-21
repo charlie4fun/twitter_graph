19ff2ea3184c92f3dd23c5fb937abaed07ca5733
charlie4fun <isakov.ilya@gmail.com>
2017-04-21 11:34:56 +0300

    mend

---
+++ load_tweets.py
@@ -0,0 +1,15 @@
+import json
+import pandas
+
+tweets_data = []
+tweets_file = open('twitter_data.txt')
+for line in tweets_file:
+    try:
+        tweet_data = json.loads(line)
+        tweets_data.append(tweet_data)
+    except:
+        continue
+
+tweets = pandas.DataFrame()
+tweets['friends'] = [tweet['user']['friends'] for tweet in tweets_data]
+tweets['friends'].max()

d0543e835fec0f97910d380548ecbabc67127aad
charlie4fun <isakov.ilya@gmail.com>
2017-04-26 11:22:03 +0300

    load_tweets fix

--- load_tweets.py
+++ load_tweets.py
@@ -1,7 +1,4 @@
 import json
-# import pandas
-
-FILES_COUNT = 14
 
 '''
 creates list of dicts containing following info about a tweet
@@ -15,29 +12,39 @@ creates list of dicts containing following info about a tweet
 }
 '''
 
-records = []
 
-for i in range(FILES_COUNT+1):
+def load_tweets(files_count):
+    records = []
 
-    tweets_data = []
-    tweets_file = open('twitter_data_%s.txt' % i)
-    for line in tweets_file:
-        try:
-            tweet_data = json.loads(line)
-            tweets_data.append(tweet_data)
-        except json.decoder.JSONDecodeError:
-            continue
+    for i in range(files_count):
 
-    for tweet in tweets_data:  # collection of all out-degree links in each tweet
-        record = {'user': tweet['user']['id'], }
-        if tweet['quoted_status']:
-            record['quoted'] = tweet['quoted_status']['user']['id']
-        if tweet['retweeted_status']:
-            record['retweeted'] = tweet['retweeted_status']['user']['id']
-        if tweet['entities']['user_mentions']:
-            record['mentioned'] = tweet['entities']['user_mentions']
-        record['replied'] = tweet.get['in_reply_to_user_id']
-        if len(record) >= 2:  # check if tweet have out-degree links at all
-            records.append(record)
+        tweets_data = []
+        tweets_file = open('twitter_data_%s.txt' % i)
+        for line in tweets_file:
+            try:
+                tweet_data = json.loads(line)
+                tweets_data.append(tweet_data)
+            except json.decoder.JSONDecodeError:
+                continue
+
+        for tweet in tweets_data:  # collection of all out-degree links in each tweet
+            record = {'user': tweet['user']['id'], }
+            if tweet.get('quoted_status'):
+                record['quoted'] = tweet['quoted_status']['user']['id']
+
+            if tweet.get('retweeted_status'):  # TODO: check if retweetee would always be in mentioned
+                record['retweeted'] = tweet['retweeted_status']['user']['id']
+
+            if tweet['entities'].get('user_mentions'):
+                mentioned = [user['id'] for user in tweet['entities']['user_mentions']]
+                record['mentioned'] = mentioned
+
+            if tweet.get('in_reply_to_user_id'):
+                record['replied'] = tweet['in_reply_to_user_id']
+
+            if len(record) >= 2:  # check if tweet have out-degree links at all
+                records.append(record)
+
+    return records
 
     # TODO: add flush to shelve-file after each txt-file {user: [in-degree links], }
